ff752e0d6ab8e38d229ebcf0759325693fa74cab
rick <technoweenie@gmail.com>
2013-03-29 19:05:55 -0300

    comments

--- multipartstreamer.go
+++ multipartstreamer.go
@@ -32,6 +32,12 @@ func New() (m *MultipartStreamer) {
   return
 }
 
+// Prepares a file and some form fields to be written to the multipart.Writer.
+// Wraps up both WriteFields() and writeFile().
+//
+// key      - The name of the field for the file data.
+// filename - The name of the file to upload.
+// fields   - A map of form field keys and values.
 func (m *MultipartStreamer) Write(key, filename string, fields map[string]string) (err error) {
   err = m.WriteFields(fields)
   if err != nil {
@@ -43,6 +49,9 @@ func (m *MultipartStreamer) Write(key, filename string, fields map[string]string
   return
 }
 
+// Writes form fields to the multipart.Writer.
+//
+// fields   - A map of form field keys and values.
 func (m *MultipartStreamer) WriteFields(fields map[string]string) error {
   var err error
 
@@ -56,6 +65,10 @@ func (m *MultipartStreamer) WriteFields(fields map[string]string) error {
   return nil
 }
 
+// Prepares a file to be written to the multipart.Writer.
+//
+// key      - The name of the field for the file data.
+// filename - The name of the file to upload.
 func (m *MultipartStreamer) WriteFile(key, filename string) (err error) {
   _, err = m.bodyWriter.CreateFormFile(key, filename)
   if err != nil {
@@ -74,16 +87,19 @@ func (m *MultipartStreamer) WriteFile(key, filename string) (err error) {
   return
 }
 
+// Sets up the http.Request body, and some crucial HTTP headers.
 func (m *MultipartStreamer) SetupRequest(req *http.Request) {
   req.Body = m.GetReader()
   req.Header.Add("Content-Type", m.ContentType)
   req.ContentLength = m.Len()
 }
 
+// Calculates the byte size of the multipart content.
 func (m *MultipartStreamer) Len() int64 {
   return m.contentLength + int64(m.bodyBuffer.Len()) + int64(m.closeBuffer.Len())
 }
 
+// Gets an io.ReadCloser for passing to an http.Request.
 func (m *MultipartStreamer) GetReader() io.ReadCloser {
   reader := io.MultiReader(m.bodyBuffer, m.fileHandle, m.closeBuffer)
   return ioutil.NopCloser(reader)
